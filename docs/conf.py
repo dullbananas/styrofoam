75730f7bb114ec867332fa47e601c59e85d21eaf
Dull Bananas <dull.bananas0@gmail.com>
2019-05-19 15:24:05 -0700

    Change version in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Dull Bananas'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.0.0a2'
+release = '1.0.0a3'
 
 
 # -- General configuration ---------------------------------------------------
