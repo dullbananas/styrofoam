142fef9b4eb9ff66c9e300412c20f63ec1818ec7
Dull Bananas <dull.bananas0@gmail.com>
2019-05-26 14:23:44 -0700

    Add test form XML parser

---
+++ tests/test_xml_parser.py
@@ -0,0 +1,18 @@
+from styrofoam import XMLParser
+
+
+class TestParser(XMLParser):
+	
+	replacements = {
+		'thing': 'url',
+		'oof': ('url1', 'url2'),
+	}
+
+
+def test_xml_relative_urls():
+	xml = '''<element>
+<thing url="path/to/thing"></thing>
+<oof url1="path/to/garbage"url2="nooo/ooo/ooo/oo"></oof>
+</element>'''
+	parser = TestParser(xml)
+	assert parser.parse() == xml
