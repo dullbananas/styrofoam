5988bb64165ea2f6b683825ec0dbeacc8e886c18
Dull Bananas <dull.bananas0@gmail.com>
2019-05-30 18:45:39 -0700

    Add remove_prefix argument to modify_url

--- styrofoam/utils.py
+++ styrofoam/utils.py
@@ -1,7 +1,30 @@
-def modify_url(url, prefix):
-	# See if the path is relative and do nothing
-	if not url.startswith('/'):
-		return url
-	# Modify the url if it is absolute
+def modify_url(url, prefix, remove_prefix=False):
+	'''Modifies a URL to go to a location withing ``prefix`` instead of ``/``.
+	Here are some examples:
+	
+	::
+	   >>> modify_url('/path/to/thing', '/prefix')
+	   '/prefix/path/to/thing'
+	   >>> modify_url('/prefix/path/to/thing', '/prefix', remove_prefix=True)
+	   '/path/to/thing'
+	   >>> modify_url('../relative/path', '/prefix')
+	   '../relative/path'
+	
+	:param url: The original URL
+	:param prefix: The URL prefix to modify the URL with. Must start with a slash
+	               but not end with one.
+	:param remove_prefix: If set to ``True``, this function will remove a prefix
+	                      instead of adding one. This assumes that the URL starts
+	                      with the prefix and is an absolute URL. Default value is
+	                      ``False``.
+	'''
+	
+	if remove_prefix:
+		# See if the path is relative and do nothing
+		if not url.startswith('/'):
+			return url
+		# Modify the url if it is absolute
+		else:
+			return prefix + url
 	else:
-		return prefix + url
+		return url[len(prefix):]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,3 +4,4 @@ from styrofoam.utils import modify_url
 def test_modify_url():
 	assert modify_url(url='relative/path', prefix='/prefix') == 'relative/path'
 	assert modify_url(url='/absolute/path', prefix='/prefix') == '/prefix/absolute/path'
+	assert modify_url('/prefix/path/to/thing', '/prefix', remove_prefix=True) == '/path/to/thing'
